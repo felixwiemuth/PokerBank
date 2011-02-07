70dabb7d4882a3c0d675ec045f7079c99d14e5d7
Felix Wiemuth <felixwiemuth@hotmail.de>
2011-02-07 15:06:17 +0100

    added main

---
+++ main.cpp
@@ -0,0 +1,17 @@
+/* 
+ * File:   main.cpp
+ * Author: Felix Wiemuth
+ *
+ * Created on 7. Februar 2011, 15:00
+ */
+
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return (EXIT_SUCCESS);
+}
+
