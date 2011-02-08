4fe58e3f7d001f1ffc7983bb50f716927f9fb16c
Felix Wiemuth <felixwiemuth@hotmail.de>
2011-02-08 18:25:08 +0100

    -

--- main.cpp
+++ main.cpp
@@ -1,17 +1,26 @@
-/* 
+/*
  * File:   main.cpp
  * Author: Felix Wiemuth
  *
  * Created on 7. Februar 2011, 15:00
  */
 
-#include <stdlib.h>
+//#include <stdlib.h>
+#include <iostream>
+
+#include "Bank.h"
+
+using namespace std;
 
 /*
- * 
+ *
  */
-int main(int argc, char** argv) {
-
-    return (EXIT_SUCCESS);
+int main(/*int argc, char** argv*/) {
+    Bank bank;
+    Bank bank2;
+    cout << "Banks balance: " << bank2.get_balance() << endl;
+    Chip c1(100, "MaanxD", 5);
+    cout << "Chip 1: " << c1;
+    return 0;
 }
 
