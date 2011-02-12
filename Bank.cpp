eaf93991c92bc81a1ec71d922b7ed4fa3ad5900f
Felix Wiemuth <felixwiemuth@hotmail.de>
2011-02-12 16:36:25 +0100

    added "Bank" class including CUI

---
+++ Bank.cpp
@@ -0,0 +1,43 @@
+/* Copyright (C) 2011 by Felix Wiemuth
+   This code is licensed under the GNU GENERAL PUBLIC LICENSE http://www.gnu.org/licenses/gpl.txt */
+
+#include "Bank.h"
+
+#include <iostream>
+
+using namespace std;
+
+Bank::Bank()
+{
+    interest = 0;
+}
+
+void Bank::run()
+{
+    cui.run();
+}
+
+int Bank::sell()
+{
+    cout << "Selling" << endl;
+}
+
+void Bank::inflation(double factor)
+{
+
+}
+
+void Bank::add_chip(Chip& chip)
+{
+    chips.push_back(chip);
+
+}
+
+int Bank::get_balance() {
+    int ret = 0;
+    for (vector<Chip>::iterator it = chips.begin(); it != chips.end(); ++it) {
+        ret += it->get_amount() * it->get_value();
+    }
+    return ret;
+}
+
---
+++ Bank.h
@@ -0,0 +1,30 @@
+/* Copyright (C) 2011 by Felix Wiemuth
+   This code is licensed under the GNU GENERAL PUBLIC LICENSE http://www.gnu.org/licenses/gpl.txt */
+
+#ifndef _BANK_H_
+#define	_BANK_H_
+
+#include "Chip.h"
+
+#include <felixwiemuth-CUI-v1.0.1-0-ga9cb660/Cui.h>
+#include <vector>
+
+
+class Bank
+{
+    public:
+        Bank();
+    private:
+        Cui<Bank> cui;
+        std::vector<Chip> chips;
+        double interest;
+    public:
+        void run(); //passes control to 'cui'
+        int sell(); //sells chips to player
+        void inflation(double factor);
+        void add_chip(Chip& chip);
+        int get_balance(); //return the value of all chips
+};
+
+#endif	/* _BANK_H */
+
