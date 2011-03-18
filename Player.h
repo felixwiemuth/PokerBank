faaa7b7438a519c8a30192e0e2db8d628b4f21ee
Felix Wiemuth <felixwiemuth@hotmail.de>
2011-03-18 16:45:33 +0100

    new class: "Player"

---
+++ Player.h
@@ -0,0 +1,9 @@
+/* Copyright (C) 2011 by Felix Wiemuth
+   This code is licensed under the GNU GENERAL PUBLIC LICENSE http://www.gnu.org/licenses/gpl.txt */
+
+#ifndef PLAYER_H_INCLUDED
+#define PLAYER_H_INCLUDED
+
+
+
+#endif // PLAYER_H_INCLUDED
