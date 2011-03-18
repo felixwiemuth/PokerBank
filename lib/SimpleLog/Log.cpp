4c73e60771606663f8345a3eff7114a382faa01d
Felix Wiemuth <felixwiemuth@hotmail.de>
2011-03-18 17:07:47 +0100

    added "SimpleLog" lib

---
+++ lib/SimpleLog/Log.cpp
@@ -0,0 +1,226 @@
+/* Copyright (C) 2011 by Felix Wiemuth
+   This code is licensed under the GNU GENERAL PUBLIC LICENSE http://www.gnu.org/licenses/gpl.txt */
+
+#include "Log.h"
+
+#include <string>
+#include <fstream>
+#include <iostream>
+
+
+using namespace std;
+
+Log::Log()
+{
+    init();
+}
+
+Log::Log(const char path[])
+{
+    init();
+    load(path);
+}
+
+void Log::init()
+{
+    remote = 0;
+    reset_configuration();
+    reset_messages();
+    log_info();
+}
+
+void Log::reset_messages()
+{
+    file_ending = ".log";
+    file_name = "log";
+    seperator = "\n";
+    output_symbol = ">>> ";
+    error_symbol = "ERR: ";
+    msg_log_info = "Logging done by SimpleLog ALPHA";
+    msg_begin_log = "--- Begin logging now ---";
+}
+
+void Log::reset_configuration()
+{
+    echo_on();
+}
+
+void Log::log_info()
+{
+    add(msg_log_info);
+    add(msg_begin_log);
+}
+
+bool Log::load(const char path[])
+{
+    logstr.clear();
+    ifstream infile(path); //create filestream to read, open file
+    if (infile.is_open() == false)
+        return false; //file not found or couldn´t be opened -> abort and return false
+
+    string line; //buffer for single lines of the file
+
+    while ( !(infile.eof()) )
+    {
+        getline(infile, line);
+        logstr.push_back(line);
+    }
+
+    infile.close(); //close filestream
+
+    return true; //return true = O.K.
+}
+
+bool Log::load()
+{
+    return load((file_name + file_ending).c_str());
+}
+
+bool Log::save(const char path[])
+{
+    ofstream file(path, ios::trunc); //create filestream to write, open file
+    if (file.is_open() == false)
+        return false; //file not opended -> abort and return false
+
+    for(vector<string>::iterator entry = logstr.begin(); entry != logstr.end(); ++entry) //write line by line into textfile
+    {
+        file << *entry << seperator;
+    }
+    file << logstr.back(); //write last line without creating new line
+
+    file.close(); //close file filestream
+
+    return true; //return true = O.K.
+}
+
+bool Log::save()
+{
+    return save((file_name + file_ending).c_str());
+}
+
+void Log::add(string s)
+{
+    //add [and print]
+    logstr.push_back(s);
+    if (echo_msg)
+        send_console();
+    //send to remote
+    if (remote != 0)
+        remote->add(s);
+}
+
+void Log::err(std::string s)
+{
+    //add [and print]
+    logstr.push_back(error_symbol + s);
+    if (echo_err)
+        send_console();
+    //send to remote
+    if (remote != 0)
+        remote->err(s);
+}
+
+void Log::echo_on()
+{
+    echo_msg = true;
+    echo_err = true;
+}
+
+void Log::echo_off()
+{
+    echo_msg = false;
+    echo_err = false;
+}
+
+void Log::echo_msg_on()
+{
+    echo_msg = true;
+}
+
+void Log::echo_msg_off()
+{
+    echo_msg = false;
+}
+
+void Log::echo_err_on()
+{
+    echo_err = true;
+}
+
+void Log::echo_err_off()
+{
+    echo_err = false;
+}
+
+void Log::set_name(std::string name)
+{
+    this->name = name;
+}
+
+void Log::set_file_ending(string file_ending)
+{
+    this->file_ending = file_ending;
+}
+void Log::set_file_name(string file_name)
+{
+    this->file_name = file_name;
+}
+void Log::set_seperator(string seperator)
+{
+    this->seperator = seperator;
+}
+void Log::set_output_symbol(string output_symbol)
+{
+    this->output_symbol = output_symbol;
+}
+void Log::set_error_symbol(string error_symbol)
+{
+    this->error_symbol = error_symbol;
+}
+void Log::set_msg_begin_log(string msg_begin_log)
+{
+    this->msg_begin_log = msg_begin_log;
+}
+
+void Log::set_remote(Log* remote_log)
+{
+    if (remote_log != this)
+        remote = remote_log;
+    else
+        remote = 0;
+}
+
+void Log::print(unsigned int entry)
+{
+    if (entry >= logstr.size())
+        return; //catching out of range
+    cout << logstr[entry] << endl;
+}
+
+void Log::print(unsigned int start, unsigned int end)
+{
+    vector<string>::iterator last;
+    if ( (end > logstr.size()-1) || (start > end) )
+        last = logstr.end();
+    else
+        last = logstr.begin() + end + 1;
+
+    for (vector<string>::iterator entry = logstr.begin() + start; entry != last; ++entry)
+    {
+        cout << *entry << endl;
+    }
+}
+
+void Log::print()
+{
+    for (vector<string>::iterator entry = logstr.begin(); entry != logstr.end(); ++entry)
+    {
+        cout << *entry << endl;
+    }
+}
+
+void Log::send_console()
+{
+    cout << name << output_symbol << logstr.back() << endl; //send every log entry to standard output
+}
+
---
+++ lib/SimpleLog/Log.h
@@ -0,0 +1,80 @@
+#ifndef LOG_H_INCLUDED
+#define LOG_H_INCLUDED
+
+#include <vector>
+#include <string>
+
+
+//class to log all program acitivity
+
+class Log
+{
+    //data
+    private:
+        std::vector<std::string> logstr;
+        //mode vars
+        bool echo_msg; //'true' = show messages in console
+        bool echo_err; //'true' = show errors in console
+        //text-vars
+        std::string name; //string to be put before every output symbol (which is before entry)
+        std::string file_ending; //standard file ending
+        std::string file_name; //standard file name
+        std::string seperator; //string to seperate log entries in log-file
+        std::string output_symbol; //string to be put before every log entry printed in the console
+        std::string error_symbol; //string to be put before every error entry
+        //messages
+        std::string msg_log_info; //information about "SimpleLog" at construction of object
+        std::string msg_begin_log; //information about starting to log
+        //remote Log -- another object of class 'Log' which should log everything this log does
+        Log* remote;
+
+    //methods declaration
+    public:
+        //constructors
+        Log(); //create empty log
+        Log(const char path[]); //create log with 'logstr' from file 'path'
+
+        void init(); //set standard values for messages / echo
+        void reset_messages(); //sets message vars to standard messages
+        void reset_configuration(); //sets configuration to default values
+        void log_info(); //logs information about Log at beginning
+
+        //methods
+        bool load(const char path[]); //loads a saved log from file 'filename' -- return value: true=ok | errors: false=file not found or could not be opened
+        bool load(); //calls 'load(const char path[])' with 'std_file_name' and 'std_file_ending'
+        bool save(const char path[]); //saves 'logstr' to file 'path' or overwrites it (!) -- return value: true=ok | errors: false=file could not be opened
+        bool save(); //calls 'save(const char path[])' with 'std_file_name' and 'std_file_ending'
+        void add(std::string s); //add normal entry
+        void err(std::string s); //add error entry
+
+        //mode switches
+        void echo_on();
+        void echo_off();
+        void echo_msg_on();
+        void echo_msg_off();
+        void echo_err_on();
+        void echo_err_off();
+
+        //set messages vars
+        void set_name(std::string name);
+        void set_file_ending(std::string file_ending);
+        void set_file_name(std::string file_name);
+        void set_seperator(std::string seperator);
+        void set_output_symbol(std::string output_symbol);
+        void set_error_symbol(std::string error_symbol);
+        void set_msg_begin_log(std::string msg_begin_log);
+
+        //set remote log (0 = no remote)
+        void set_remote(Log* remote_log); //sets 'remote' to 'remote_log' (if 'remote_log' != 'this')
+
+        //print methods
+        void print(unsigned int entry); //displays 'logstr[entry]'
+        void print(unsigned int start, unsigned int end); //displays 'logstr[start]' to 'logstr[end]' -- if end<start logstr[start] to logstr[entries] will be displayes
+        void print(); //displays whole content of 'logstr'
+
+    private:
+        void send_console(); //sends last entry to standard output -- used to print new log entry
+};
+
+
+#endif // LOG_H_INCLUDED
