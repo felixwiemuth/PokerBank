felixwiemuth/PokerBank

commit 95dd1d86155d3122d6a681416df8c2a243580693

added tracking of how many money is under players

Bank.cpp
@@ -16,6 +16,7 @@ Bank::Bank()
     interest_buy = 0;
     interest_sell = 0;
     money = 0;
+    chips_players = 0;
     log.set_remote(&syslog);
     log.echo_off();
     syslog.set_file_name("syslog");
@@ -335,7 +336,7 @@ void Bank::deal_chips_all_players(vector< pair<int, int> > chips)
 {
     log << "Change amount of chips at each player as following:\nAdd: ";
     stringstream take;
-    int diffbank = 0;
+    int diff = 0;
     for (vector< pair<int, int> >::iterator c = chips.begin(); c != chips.end(); ++c)
     {
         if (!change_chip_amount(c->second, -(c->first * players.size())))
@@ -346,9 +347,11 @@ void Bank::deal_chips_all_players(vector< pair<int, int> > chips)
             take << " " << -(c->first) << "x" << c->second;
         else
             continue;
-        diffbank -= c->first * c->second;
+        diff -= c->first * c->second;
     }
-    log << "\nTake:" << take.str() << "\nResult player: " << -diffbank << "  ***  Result bank: " << diffbank * int(players.size());
+    int diffbank = diff * int(players.size());
+    chips_players -= diffbank; //update 'chips_players'
+    log << "\nTake:" << take.str() << "\nResult player: " << -diff << "  ***  Result bank: " << diffbank;
     log.add();
 }
 
@@ -384,6 +387,12 @@ string Bank::get_interest_sell()
 void Bank::show_money()
 {
     log << "Money in bank: " << money;
+    log.add();
+}
+
+void Bank::show_chips_players()
+{
+    log << "Value of all chips belonging to players: " << chips_players;
     log.add();
 }
 
@@ -465,7 +474,7 @@ vector< pair<int, int> > Bank::str_to_chips(vector<string>::iterator first, vect
 
 void Bank::buy_sell(bool buy, string name, vector< pair<int, int> > buychips)
 {
-    int brutto = 0;
+    int brutto;
     vector<Player>::iterator p = check_player(name);
     if (p != players.end())
         log << *p;
@@ -496,10 +505,14 @@ void Bank::buy_sell(bool buy, string name, vector< pair<int, int> > buychips)
         else
             chips[it->second].increase_amount(it->first);
 
-        //update log entry
-        int add = it->first * it->second;
-        brutto += add;
-        log << " " << it->first << "x" << it->second << " (" << add << ");";
+        brutto = it->first * it->second;
+        log << " " << it->first << "x" << it->second << " (" << brutto << ");";
+
+        //update 'chips_players'
+        if (buy)
+            chips_players += brutto;
+        else
+            chips_players -= brutto;
     }
     //put log entry
     if (brutto != 0)

Bank.h
@@ -33,6 +33,7 @@ class Bank
         Bank();
     private:
         int money; //money belonging to the bank
+        int chips_players; //value of all chips currently belonging to players (is only correct if every chip transfer is contorlled by bank)
         std::vector<Player> players; //TODO in methods below players can write #XXX instead of name, with "XXX" being their registered id or name
         Log syslog; //log to log everything
         Log log; //log to log bank activity
@@ -72,6 +73,7 @@ class Bank
         std::string get_interest_sell(); //returns 'interest_sell' as string in form "xx%"
         //methods to show (log) status information
         void show_money();
+        void show_chips_players();
         void show_interest();
         void show_chip_value();
         void show_chips();

commit a07da4a798e123a1b366a929c67d437ded64a252

fixed bug caused by commit 95dd1d8: handling of result in "buy_sell"

Bank.cpp
@@ -470,7 +470,6 @@ vector< pair<int, int> > Bank::str_to_chips(vector<string>::iterator first, vect
         {
             value = boost::lexical_cast<int>(n[1]);
         }
-
         catch (boost::bad_lexical_cast&)
         {
             //check if 'n[1]' contains the name of a chip sort
@@ -491,7 +490,7 @@ vector< pair<int, int> > Bank::str_to_chips(vector<string>::iterator first, vect
 
 void Bank::buy_sell(bool buy, string name, vector< pair<int, int> > buychips)
 {
-    int brutto;
+    int brutto = 0;
     vector<Player>::iterator p = check_player(name);
     if (p != players.end())
         log << *p;
@@ -522,14 +521,10 @@ void Bank::buy_sell(bool buy, string name, vector< pair<int, int> > buychips)
         else
             chips[it->second].increase_amount(it->first);
 
-        brutto = it->first * it->second;
-        log << " " << it->first << "x" << it->second << " (" << brutto << ");";
-
-        //update 'chips_players'
-        if (buy)
-            chips_players += brutto;
-        else
-            chips_players -= brutto;
+        //update log entry
+        int add = it->first * it->second;
+        brutto += add;
+        log << " " << it->first << "x" << it->second << " (" << add << ");";
     }
     //put log entry
     if (brutto != 0)
@@ -542,6 +537,7 @@ void Bank::buy_sell(bool buy, string name, vector< pair<int, int> > buychips)
             netto = brutto + interest;
             money += netto;
             log << "\nResult: " << brutto << "  ***  Interest(" << get_interest_buy() << "): " << interest << "  ***  Please pay: ";
+            chips_players += brutto;
         }
         else
         {
@@ -549,6 +545,7 @@ void Bank::buy_sell(bool buy, string name, vector< pair<int, int> > buychips)
             netto = brutto - interest;
             money -= netto;
             log << "\nResult: " << brutto << "  ***  Interest(" << get_interest_sell() << "): " << interest << "  ***  You get: ";
+            chips_players -= brutto;
         }
         log << netto;
         log.add();